294a0aebd0a3951cdd5b3aafbf7470755ee8ebe6
tjuan <jf.danieltampubolon@gmail.com>
2022-01-07 15:27:22 -0500

    updated translations

---
+++ Translations.js
@@ -0,0 +1,29 @@
+/* Traducción Español
+Horas! (¡Hola!),
+Soy Juan Tampubolon.
+/huan tappubolon/
+Soy de Indonesia originalmente, pero cuando tuvé 16 años fue á los Estados Unidos
+para terminar mi escuela secundaria y obtener una licenciatura en lingüística en la ciencía de computación. Actualmente,
+estoy terminando mi licenciatura en la Universidad de Michigan. Buscará una oppurtunidad para trabajar después de
+la primavera que viene.
+
+Traduction Français
+Horas!(Salut!),
+Moi c'est Juan Tampubolon,
+/huan tappubolon/
+Je suis orignaire d'Indonésie, mais quand j'avais 16 ans j'ai démenagé aux états-Unis pour finir mon lycée. Actuellement,
+je suis dans mon dérnière semestre à l'Univérsité de Michigan où je vais gagner un baccalauréat en linguistique et le science d'ordinateur.
+Je cherche des opportunité après l'obtention du diplôme.
+
+Terjemahan Bahasa Indonesia
+Horas! (Halo!), 
+Perkenalkan saya Juan Tampubolon
+/huan tappubolon/
+Saya berasal dari Indonesia, tetapi ketika saya berumur 16 saya merantau ke Amerika Serikat untuk
+menyelesaikan pendidikan SMA saya. Sesudah mendapatkan ijazah SMA dari Washington State, saya juga mendapatkan
+Associate's degree. Sekarang, saya sedang menjalani semester ke-8 di Universitas Michigan (University of Michigan)
+dan saya terbuka untuk menerima tawaran kerja setelah wisuda nanti.
+
+Make an interactive CV
+with skill bars that are annimated
+*/
